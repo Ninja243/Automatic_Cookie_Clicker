81e21b825d1de03146b222421ae8d87b8646f992
Mweya Ruider <mweyaruider@gmail.com>
2019-03-09 17:32:06 +0200

    Create autoclicker.js

---
+++ autoclicker.js
@@ -0,0 +1,62 @@
+function runclicker() {
+
+  // Select the cookie to click
+  var x = document.getElementById('bigCookie');
+  // Click the cookie
+  function spam() {
+    x.click(); 
+    setInterval(spam, 10);
+  }; 
+  // Run the cookie clicking function
+  spam();
+  
+  // Buy the things
+  function abs() {  
+    // Select all products that we can afford
+    var b = document.getElementById('products').getElementsByClassName('unlocked enabled');  
+    // Click on everything we can afford
+    // Misclicks don't matter
+    var j = 0;
+    while (j<b.length) {
+      b[j].click();
+      j = j+1;
+    };  
+    // Select all upgrades that we can afford
+    b = document.getElementById('upgrades').getElementsByClassName('crate enabled'); 
+    // Click on everything we can afford
+    // Misclicks don't matter
+    var j = 0;
+    while (j<b.length) {
+      b[j].click();
+      j = j+1;
+    };   
+     // Select all tech upgrades that we can afford
+    b = document.getElementById('techUpgrades').getElementsByClassName('crate enabled');    
+    // Click on everything we can afford
+    // Misclicks don't matter
+    var j = 0;
+    while (j<b.length) {
+      b[j].click();
+      j = j+1;
+    };
+    setInterval(abs, 30);
+  };  
+  // Run the auto buy function
+  abs();
+  
+  // Handle special cookies
+  function pu() {
+    var c = document.getElementsByClassName('shimmer');
+    var j = 0;
+    while (j<c.length) {
+      c[j].click();
+      j = j+1;
+    };
+    setInterval(pu, 500);
+  }  
+  // Run the special cookie function
+  pu();
+  
+}; 
+
+runclicker();
